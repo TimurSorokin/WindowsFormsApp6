2ea6a62f25381347178a7f9e5872c4938bf19429
TimurSorokin <70172615+TimurSorokin@users.noreply.github.com>
2020-09-15 19:25:30 +0200

    Initial commit

---
+++ WindowsFormsApp6/Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace WindowsFormsApp6
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.show = new System.Windows.Forms.Button();
+            this.addimage = new System.Windows.Forms.Button();
+            this.find = new System.Windows.Forms.Button();
+            this.addfolder = new System.Windows.Forms.Button();
+            this.console = new System.Windows.Forms.RichTextBox();
+            this.console2 = new System.Windows.Forms.RichTextBox();
+            this.stats = new System.Windows.Forms.Label();
+            this.folderBrowserDialog1 = new System.Windows.Forms.FolderBrowserDialog();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.findTextBox = new System.Windows.Forms.TextBox();
+            this.findOk = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // show
+            // 
+            this.show.Location = new System.Drawing.Point(187, 277);
+            this.show.Name = "show";
+            this.show.Size = new System.Drawing.Size(75, 23);
+            this.show.TabIndex = 0;
+            this.show.Text = "Show Images";
+            this.show.UseVisualStyleBackColor = true;
+            this.show.Click += new System.EventHandler(this.show_Click);
+            // 
+            // addimage
+            // 
+            this.addimage.Location = new System.Drawing.Point(25, 277);
+            this.addimage.Name = "addimage";
+            this.addimage.Size = new System.Drawing.Size(75, 23);
+            this.addimage.TabIndex = 1;
+            this.addimage.Text = "Add Image";
+            this.addimage.UseVisualStyleBackColor = true;
+            this.addimage.Click += new System.EventHandler(this.add_Click);
+            // 
+            // find
+            // 
+            this.find.Location = new System.Drawing.Point(268, 277);
+            this.find.Name = "find";
+            this.find.Size = new System.Drawing.Size(75, 23);
+            this.find.TabIndex = 2;
+            this.find.Text = "Find Image";
+            this.find.UseVisualStyleBackColor = true;
+            this.find.Click += new System.EventHandler(this.find_Click);
+            // 
+            // addfolder
+            // 
+            this.addfolder.Location = new System.Drawing.Point(106, 277);
+            this.addfolder.Name = "addfolder";
+            this.addfolder.Size = new System.Drawing.Size(75, 23);
+            this.addfolder.TabIndex = 4;
+            this.addfolder.Text = "Add Folder";
+            this.addfolder.UseVisualStyleBackColor = true;
+            this.addfolder.Click += new System.EventHandler(this.addfolder_Click);
+            // 
+            // console
+            // 
+            this.console.BackColor = System.Drawing.Color.LightSteelBlue;
+            this.console.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.console.Font = new System.Drawing.Font("Corbel", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.console.ForeColor = System.Drawing.Color.Black;
+            this.console.Location = new System.Drawing.Point(12, 12);
+            this.console.Name = "console";
+            this.console.Size = new System.Drawing.Size(879, 256);
+            this.console.TabIndex = 5;
+            this.console.Text = "";
+            this.console.TextChanged += new System.EventHandler(this.console_TextChanged);
+            // 
+            // console2
+            // 
+            this.console2.Location = new System.Drawing.Point(738, 274);
+            this.console2.Name = "console2";
+            this.console2.Size = new System.Drawing.Size(153, 96);
+            this.console2.TabIndex = 6;
+            this.console2.Text = "";
+            // 
+            // stats
+            // 
+            this.stats.AutoSize = true;
+            this.stats.Location = new System.Drawing.Point(687, 277);
+            this.stats.Name = "stats";
+            this.stats.Size = new System.Drawing.Size(45, 13);
+            this.stats.TabIndex = 7;
+            this.stats.Text = "STATS:";
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // findTextBox
+            // 
+            this.findTextBox.Location = new System.Drawing.Point(268, 306);
+            this.findTextBox.Name = "findTextBox";
+            this.findTextBox.Size = new System.Drawing.Size(100, 20);
+            this.findTextBox.TabIndex = 8;
+            // 
+            // findOk
+            // 
+            this.findOk.Location = new System.Drawing.Point(374, 303);
+            this.findOk.Name = "findOk";
+            this.findOk.Size = new System.Drawing.Size(40, 23);
+            this.findOk.TabIndex = 9;
+            this.findOk.Text = "Find";
+            this.findOk.UseVisualStyleBackColor = true;
+            this.findOk.Click += new System.EventHandler(this.findOk_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(903, 388);
+            this.Controls.Add(this.findOk);
+            this.Controls.Add(this.findTextBox);
+            this.Controls.Add(this.addfolder);
+            this.Controls.Add(this.find);
+            this.Controls.Add(this.show);
+            this.Controls.Add(this.stats);
+            this.Controls.Add(this.addimage);
+            this.Controls.Add(this.console2);
+            this.Controls.Add(this.console);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button show;
+        private System.Windows.Forms.Button addimage;
+        private System.Windows.Forms.Button find;
+        private System.Windows.Forms.RichTextBox console;
+        private System.Windows.Forms.RichTextBox console2;
+        private System.Windows.Forms.Label stats;
+        private System.Windows.Forms.FolderBrowserDialog folderBrowserDialog1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.Button addfolder;
+        private System.Windows.Forms.TextBox findTextBox;
+        private System.Windows.Forms.Button findOk;
+    }
+}
+
---
+++ WindowsFormsApp6/Form1.cs
@@ -0,0 +1,343 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.Security.Policy;
+
+namespace WindowsFormsApp6
+{
+    public partial class Form1 : Form
+    {
+        imagedatabase imagedatabase = new imagedatabase();
+        
+        public Form1()
+        {
+           
+            InitializeComponent();
+            findTextBox.Visible = false;
+            findOk.Visible = false;
+
+        }
+
+        private void add_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.ShowDialog();
+            string path = openFileDialog1.FileName;
+            AddFileOrFolder(path);
+            console2.Text = imagedatabase.DataBaseStats();
+        }
+
+        private  void AddFileOrFolder(string path)
+        {
+            
+            
+            Console.WriteLine(Checker(path));
+
+           
+            string filename;
+            int imgHeight;
+            int imgWidth;
+            long length;
+            string output;
+            image image;
+            switch (Checker(path))
+            {
+                case ObjectType.file:
+
+                    if(ImageChecker(path) != ImageType.undefined)
+                    {
+
+                    
+
+                      var imgfromfile = Image.FromFile(path);
+                           filename = Path.GetFileName(path);
+                            imgHeight = imgfromfile.Height;
+                            imgWidth = imgfromfile.Width;
+                            length = new System.IO.FileInfo(path).Length;
+                            output = String.Format("Name: {0} | Width: {1} | Heght: {2} | Size: {3} KB \n ", filename, imgHeight, imgWidth, length/1000);
+
+                            console.Text += output;
+                           // console.Text += (a + "||| TYPE|" + ImageChecker(a));
+                                                
+                            image =  new image(filename, imgWidth, imgHeight, length/1000);
+                            imagedatabase.AddToDataBase(image);
+                    }
+                    else
+                    {
+                        MessageBox.Show("Path doesnt contain images");
+                    }
+
+                    break;
+
+
+                case ObjectType.folder:
+                    string[] test = Directory.GetFiles(path);
+                    
+                    foreach (string a in test)
+                    {
+
+                        if (ImageChecker(a) != ImageType.undefined)
+                        {
+                           
+
+                            var img = Image.FromFile(a);
+                            filename = Path.GetFileName(a);
+                            imgHeight = img.Height;
+                            imgWidth = img.Width;
+                            length = new System.IO.FileInfo(a).Length;
+                            output = String.Format("Name: {0} | Width{1} | Heght {2} | Size {3} KB \n \n \n", filename, imgHeight, imgWidth, length/1000);
+
+                            console.Text += output;
+                           // console.Text += (a + "||| TYPE|" + ImageChecker(a));
+                                                
+                            image =  new image(filename, imgWidth, imgHeight, length/1000);
+                            imagedatabase.AddToDataBase(image);
+                           
+                          
+                            
+                        }
+                        else
+                        {
+                            MessageBox.Show("Path doesnt contain images");
+                        }
+                        
+
+                    }
+                 
+                    break;
+                case ObjectType.uknown:
+
+                    MessageBox.Show("Something went wrong!");
+                    break;
+                    
+            }
+
+        }
+
+        enum ImageType
+        {
+            bmp,
+            png,
+            jpg,
+            tiff,
+            tiff2,
+            undefined
+
+        }
+        private static ImageType ImageChecker(string path)
+        {
+            FileStream fs = new FileStream(path, FileMode.Open, FileAccess.Read);
+            byte[] bytes = new byte[8];
+            int fsRead = fs.Read(bytes, 0, 8);
+            byte[] bmp = { 0x42, 0x4D };
+            byte[] jpg = { 255, 216, 255, 224 };
+            byte[] jpgcanon = { 255, 216, 255, 255 };
+            byte[] png = { 137, 80, 78, 71, 13, 10, 26, 10 };
+            byte[] tiff = { 73, 73, 42 };
+            byte[] tiff2 = { 77, 77, 42 };
+
+
+            //bmp
+            if (Enumerable.SequenceEqual(bmp, bytes.Take(bmp.Length)))
+            {
+                return ImageType.bmp;
+            }
+            //png
+            else if (Enumerable.SequenceEqual(png, bytes.Take(png.Length)))
+            {
+                return ImageType.png;
+            }
+            //jpg
+            else if (Enumerable.SequenceEqual(jpg, bytes.Take(jpg.Length)))
+            {
+                return ImageType.jpg;
+            }
+            //jpg canon
+            else if (Enumerable.SequenceEqual(jpgcanon, bytes.Take(jpgcanon.Length)))
+            {
+                return ImageType.jpg;
+            }
+            //tiff
+            else if (Enumerable.SequenceEqual(tiff, bytes.Take(tiff.Length)))
+            {
+                return ImageType.tiff;
+            }
+            //tiff2
+            else if (Enumerable.SequenceEqual(tiff2, bytes.Take(tiff2.Length)))
+            {
+                return ImageType.tiff2;
+            }
+
+            return ImageType.undefined;
+        }
+
+
+        enum ObjectType
+        {
+            folder,
+            file,
+            uknown
+        }
+
+        private static ObjectType Checker(string path)
+        {
+
+            if (File.Exists(path))
+            {
+                //file
+
+
+                return ObjectType.file;
+            }
+            else if (Directory.Exists(path))
+            {
+                //dir
+                return ObjectType.folder;
+            }
+            else
+            {
+                return ObjectType.uknown;
+            }
+        }
+
+        private void console_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void show_Click(object sender, EventArgs e)
+        {
+
+            console.Text = "";
+            console.Text += imagedatabase.ShowDataBase();
+            console2.Text = imagedatabase.DataBaseStats();
+            
+        }
+
+        private void exit_Click(object sender, EventArgs e)
+        {
+            System.Environment.Exit(1);
+        }
+
+        private void openFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void addfolder_Click(object sender, EventArgs e)
+        {
+           
+            folderBrowserDialog1.ShowDialog();
+            string path = folderBrowserDialog1.SelectedPath;
+            AddFileOrFolder(path);
+            console2.Text = imagedatabase.DataBaseStats();
+        }
+
+        private void find_Click(object sender, EventArgs e)
+        {
+            findTextBox.Visible = true;
+            findOk.Visible = true;
+        }
+
+        private void findOk_Click(object sender, EventArgs e)
+        {
+            console.Text = imagedatabase.FindImageInDataBase(findTextBox.Text);
+            findTextBox.Visible = false;
+            findOk.Visible = false;
+        }
+    }
+    class image
+    {
+        public string filename;
+        public int imgHeight;
+        public int imgWidth;
+        public long length;
+
+        public image(string name,int width, int height,long length)
+        {
+            this.filename = name;
+            this.imgHeight = height;
+            this.imgWidth = width;
+            this.length = length;
+        }
+    }
+
+    class imagedatabase
+    {
+        List<image> database;
+        public imagedatabase()
+        {
+            database = new List<image>(700);
+        }
+
+        public void AddToDataBase(image image)
+        {
+            if((database.Capacity-database.Count)<10)
+            {
+                MessageBox.Show("Less than 10 slosts");
+            }
+            
+            if(database.Count< database.Capacity)
+            {
+                database.Add(image);
+
+            }
+            else
+            {
+                MessageBox.Show("No more space");
+            }
+
+        }
+        public string ShowDataBase()
+        {
+            string output = "";
+
+            foreach (image show in database)
+            {
+                output += String.Format("Name: {0} | Width{1} | Heght {2} | Size {3} KB \n \n \n", show.filename, show.imgHeight, show.imgWidth, show.length);
+                
+            }
+            return output;
+        }
+        public string DataBaseStats()
+        {
+            long totallenght=0;
+            int imgcount = 0;
+            int slots = database.Capacity;
+            int slotsused = database.Count;
+
+            foreach(image count in database)
+            {
+                totallenght += count.length;
+                imgcount++;
+            }
+
+            string output = String.Format("Total images: {0}\nTotal size(kb): {1}\nTotal slots: {2}/{3}", imgcount, totallenght,slotsused,slots);
+            return output;
+        }
+        public string FindImageInDataBase(string name)
+        {
+            int position = 0;
+            string output="";
+
+            foreach(image find in database)
+            {
+                if(name == find.filename)
+                {
+                    output = String.Format("File found at position: {0} \nName: {1} | Width{2} | Height {3} | Size {4} KB \n \n \n", position, find.filename, find.imgWidth, find.imgHeight, find.length);
+                    break;
+
+                    
+                }
+                
+                position++;
+            }
+            return output;
+        }
+    }
+}
